51a9a968b203564a567d6def2bcc66ba9fd01d44
stephenedunne <97405617+stephenedunne@users.noreply.github.com>
2023-11-25 21:04:57 +0000

    Upload notionational_converter.py from local host via git

---
+++ notational_converter.py
@@ -0,0 +1,49 @@
+"""Program to convert a user input's into their requested notational system."""
+
+def binary_to_hex(user_input):
+    """
+    Converts user's binary value to hexadecimal.
+    """
+    pass
+
+def binary_to_decimal(user_input):
+    """
+    Converts user's binary value to decimal.
+    """
+    pass
+
+def hex_to_binary(user_input):
+    """
+    Converts user's hexadecimal value to binary.
+    """
+    pass
+
+def hex_to_decimal(user_input):
+    """
+    Converts user's hexadecimal value to decimal.
+    """
+    pass
+
+def decimal_to_binary(user_input):
+    """
+    Converts user's decimal value to binary.
+    """
+    pass
+
+def decimal_to_hex(user_input):
+    """
+    Converts user's decimal value to hexadecimal.
+    """
+    pass
+
+def main():
+    """
+    1. Asks the user which notational system they'd like to convert from
+    2. Asks the user which notational system they'd like to convert to
+    3. Passes their input to the correct function
+    4. Returns the input in the requested notational system
+    """
+    user_input = input("What would you like to convert?")
+
+if __name__ == "__main__":
+    main()
